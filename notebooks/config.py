b1eaf8b9d0559712c89e1e585a18ff2decb32690
Suhas G <suhas.g96.sg@gmail.com>
2022-07-18 14:35:54 +0200

    working classification

---
+++ notebooks/config.py
@@ -0,0 +1,10 @@
+DATA_FOLDER = './data/'
+DATA_FILES = {
+    'idle': ['Custom Data - 2022-07-14 - idle (1).csv','Custom Data - 2022-07-14 - idle 2.csv', 'Custom Data - 2022-07-14 - idle 3.csv'],
+    'jogging': ['Custom Data - 2022-07-14 - jogging in place 2.csv', 'Custom Data - 2022-07-14 - jogging in place 3.csv', 
+                'Custom Data - 2022-07-14 - jogging in place.csv', 'Custom Data - 2022-07-14 - Jogging_M.csv'],
+    'after_exercise': ['Custom Data - 2022-07-14 - after exercise 2.csv', 'Custom Data - 2022-07-14 - Recording 1afrervexer.csv'],
+    'walking': ['Custom Data - 2022-07-14 - walking 1.csv', 'Custom Data - 2022-07-14 - walking 2.csv'],
+    'exercise': ['Custom Data - 2022-07-07 - DiveshKumar_Exercise.csv', 'Custom Data - 2022-07-07 - Suhas_Exercise.csv', 'gan_DataExercise.csv']
+}
+DESTINATION_FOLDER = './preprocessed/'
